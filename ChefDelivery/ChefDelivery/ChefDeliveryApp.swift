1d452f2ae0ad30e89ecc9ffce2ccb0d863df07b3
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-03 12:54:09 -0300

    VStack {             Text("Teste")             Text("Teste")         }

---
+++ ChefDelivery/ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Natasha Brandão on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDelivery/ChefDelivery/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by Natasha Brandão on 03/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Teste")
+            Text("Teste")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewLayout(.sizeThatFits)
+    }
+}
