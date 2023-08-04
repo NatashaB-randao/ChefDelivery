90edd9b5e75997eae771b2e6d2a33807a2e491b8
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-04 13:16:21 -0300

    Corrigindo o layout modificadores de objetos

--- ChefDelivery/ChefDelivery/NavigationBar/NavigationBar.swift
+++ ChefDelivery/ChefDelivery/NavigationBar/NavigationBar.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct NavigationBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            Button("R. Vergueiro, 3185") {
+                
+            }
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image(systemName: "bell.badge")
+                    .font(.title3)
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 
 struct NavigationBar_Previews: PreviewProvider {
     static var previews: some View {
         NavigationBar()
+            .previewLayout(.sizeThatFits)
+            .padding()
     }
 }
