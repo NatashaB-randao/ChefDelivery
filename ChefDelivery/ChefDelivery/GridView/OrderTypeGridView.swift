206beaad1f6edd0b159e079567a6b6ae59c9993b
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-05 14:54:25 -0300

    Criando mocks

--- ChefDelivery/ChefDelivery/GridView/OrderTypeGridView.swift
+++ ChefDelivery/ChefDelivery/GridView/OrderTypeGridView.swift
@@ -8,16 +8,15 @@
 import SwiftUI
 
 struct OrderTypeGridView: View {
-    
-    let orders = ["Restaurantes", "Farmácia", "Descontos", "Gourmet", "Mercados","Pet", "Bebidas"]
+   
     
     var body: some View {
         LazyHGrid(rows: [
             GridItem(.fixed(100)),
             GridItem(.fixed(100))
         ]) {
-            ForEach(orders, id: \.self) { orderItem in
-                Text(orderItem)
+            ForEach(ordersMock) { orderItem in
+                Text(orderItem.name)
             }
         }
     }
--- ChefDelivery/ChefDelivery/Model/DataSourceMock.swift
+++ ChefDelivery/ChefDelivery/Model/DataSourceMock.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1, name: "Restaurantes", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercados", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmacia"),
+    OrderType(id: 4, name: "Pet", image: "petshop"),
+    OrderType(id: 5, name: "Descontos", image: "descontos"),
+    OrderType(id: 6, name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7, name: "Gourmet", image: "gourmet"),
+
+    
+]
