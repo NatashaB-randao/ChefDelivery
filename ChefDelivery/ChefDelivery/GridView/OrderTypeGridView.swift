b616aa606db463c5a08fb230cae4903e02b619d9
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-08 13:24:20 -0300

    Utilizando GridItem

--- ChefDelivery/ChefDelivery/GridView/OrderTypeGridView.swift
+++ ChefDelivery/ChefDelivery/GridView/OrderTypeGridView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct OrderTypeGridView: View {
-   
+    
+    var gridLayout: [GridItem] {
+        return Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+    }
     
     var body: some View {
-        LazyHGrid(rows: [
-            GridItem(.fixed(100)),
-            GridItem(.fixed(100))
-        ]) {
+        LazyHGrid(rows: gridLayout, spacing: 15) {
             ForEach(ordersMock) { orderItem in
-                Text(orderItem.name)
+                OrderTypeView(orderType: orderItem)
             }
         }
     }
