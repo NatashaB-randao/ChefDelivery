7e314c003b19eb014bc14cf948f8d82bc349bcbe
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-11 13:41:25 -0300

    Preparando e refatorando o projeto para receber as novas funcionalidades

---
+++ ChefDelivery/ChefDelivery/Model/ProductType.swift
@@ -0,0 +1,16 @@
+//
+//  ProductType.swift
+//  ChefDelivery
+//
+//  Created by Natasha Brandão on 11/08/23.
+//
+
+import Foundation
+
+struct ProductType: Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let image: String
+    let price: Double
+}
--- ChefDelivery/ChefDelivery/StoresView/StoreItemView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoreItemView.swift
@@ -9,18 +9,18 @@ import SwiftUI
 
 struct StoreItemView: View {
     
-    let order: OrderType
+    let store: StoreType
     
     var body: some View {
         HStack {
-            Image(order.image)
+            Image(store.logoImage)
                 .resizable()
                 .scaledToFit()
                 .cornerRadius(25)
                 .frame(width: 50, height: 50)
             
             VStack {
-                Text(order.name)
+                Text(store.name)
                     .font(.subheadline)
             }
             
@@ -31,7 +31,7 @@ struct StoreItemView: View {
 
 struct StoreItemView_Previews: PreviewProvider {
     static var previews: some View {
-        StoreItemView(order: OrderType(id: 1, name: "Monstro Burguer", image: "monstro-burger-logo"))
+        StoreItemView(store: storesMock[0])
             .previewLayout(.sizeThatFits)
     }
 }
