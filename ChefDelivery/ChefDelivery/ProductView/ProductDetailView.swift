12e9391080aecc1dc75ee392b9745ba4c04336ff
NatashaB-randao <natashabrandao57@hotmail.com>
2023-08-17 13:00:04 -0300

    Criando layout da tela de produtos

--- ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
+++ ChefDelivery/ChefDelivery/ProductView/ProductDetailView.swift
@@ -12,7 +12,44 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
-        Text(product.name)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 20)
+                
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(product.description)
+                    .padding(.horizontal)
+                
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+            
+            Spacer()
+            
+            Button {
+                print("O botão foi pressionado")
+            } label: {
+                HStack {
+                    Image(systemName: "cart")
+                    
+                    Text("Adicionar ao carrinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical, 16)
+                .font(.title3)
+                .bold()
+            }
+
+        }
     }
 }
 
